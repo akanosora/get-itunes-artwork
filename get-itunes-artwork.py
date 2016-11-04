617082e4be0bd6d744d0f63f9f2ac7065ca503a3
Zhen-Huan (Kenny) Hu <zhu@mcw.edu>
2016-11-04 16:56:07 -0500

    Use unique filenames when searching a list of titles

--- get-itunes-artwork.py
+++ get-itunes-artwork.py
@@ -5,6 +5,9 @@ import os, sys
 import urllib, urllib2
 import json
 
+SAVE_TO = "%s/Desktop/" % os.path.expanduser("~") # Directory must exist
+TITLES = [] # Optionally populate this with a list of titles for batch processing
+
 MOVIES = {
     'title_prompt': "Search for a movie title (or type '#help' to see more options): ",
     'entity': 'movie',
@@ -19,27 +22,24 @@ TV = {
 media = MOVIES.copy()
 media.update({'country': 'US'})
 
-SAVE_TO = "%s/Desktop/" % os.path.expanduser("~") # Directory must exist
-TITLES = [] # Optionally populate this with a list of titles for batch processing
-
-def get_art(title=None, keep_going=False):
+def get_art(title=None, keep_going=False, unique_filenames=True):
     global not_found, media
     if not title:
         title = raw_input(media['title_prompt'])
         if title == '':
-            get_art(None, True)
+            get_art(None, True, False)
         if title == '#movie':
             media.update(MOVIES)
-            get_art(None, True)
+            get_art(None, True, False)
         elif title == '#tvshow':
             media.update(TV)
-            get_art(None, True)
+            get_art(None, True, False)
         elif title == '#country':
             media.update({'country': raw_input('Type two-letter country code for the store you want to search: ')})
-            get_art(None, True)
+            get_art(None, True, False)
         elif title == '#help':
-            print "Type '#movie' to switch to searching for movie titles\nType '#tvshow' to switch to searching for TV shows\nType '#country' to change country (The default is US)\nType '#quit' to exit the program"
-            get_art(None, True)
+            print "\nType '#movie' to switch to searching for movie titles\nType '#tvshow' to switch to searching for TV shows\nType '#country' to change country (The default is US)\nType '#quit' to exit the program\n"
+            get_art(None, True, False)
         elif title == '#quit':
             exit();
     
@@ -65,8 +65,11 @@ def get_art(title=None, keep_going=False):
                     not_found.append(title)
             if which != None:
                 url = results['results'][which]['artworkUrl100'].replace("100x100bb.jpg", "1200x1200bb.png")
-                sys.stdout.write("Downloading artwork...") 
-                urllib.urlretrieve(url, "%s/Poster.png" % SAVE_TO)
+                sys.stdout.write("Downloading artwork...")
+                if unique_filenames:
+                    urllib.urlretrieve(url, "%s/%s.png" % (SAVE_TO, title.replace("/", "-").replace(":", "_")))
+                else:
+                    urllib.urlretrieve(url, "%s/Poster.png" % SAVE_TO)
                 sys.stdout.write(" done.\n\n")
             
         else:
@@ -85,7 +88,7 @@ if __name__ == "__main__":
         for title in TITLES:
             get_art(title)
     else:
-        get_art(None, True)
+        get_art(None, True, False)
     print "\n\nArtwork was not found for the following titles:"
     for title in not_found:
         print title
